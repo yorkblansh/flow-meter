4d9a4b73d9c4741880013600d01182de9760a275
Oleg Chepelev <yorkblansh@yandex.com>
2024-02-08 13:14:06 +0300

    upd

--- app/workspaces/react/src/App.tsx
+++ app/workspaces/react/src/App.tsx
@@ -3,69 +3,38 @@ import _ from 'lodash'
 import { useEffect, useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { supabase } from './client'
+import { NumericInput } from './widgets/NumericInput'
+import { useFocus } from './shared/hooks/useFocus'
 
-const App = () => {
-	// useEffect(() => {
+const Counters = () => {
+	const [inputRef, setInputFocus] = useFocus()
+	const [indexInput, setIndexInput] = useState('')
 
-	// }, [])
-
-	// return (
-	// 	<div>
-	// 		<div className=""></div>
-	// 	</div>
-	// )
-
-	const [user, setUser] = useState(null)
 	useEffect(() => {
-		checkUser()
-		window.addEventListener('hashchange', function () {
-			checkUser()
+		window.addEventListener('keypress', (e) => {
+			setInputFocus()
 		})
 	}, [])
 
-	const getSession = async () => {
-		// supabase
-		// 	.schema('public').
-		supabase
-			.from('test_table')
-			.select()
-			.then((d) => {
-				// console.log(d.error)
-				console.log(d.data)
-			})
-		// console.log((await supabase.auth.getSession()).data.session.)
-	}
-
-	async function checkUser() {
-		const user = await supabase.auth.getUser()
-		setUser(user.data.user)
-	}
-	async function signInWithGithub() {
-		await supabase.auth.signInWithOAuth({
-			provider: 'github',
-		})
-	}
-	async function signOut() {
-		await supabase.auth.signOut()
-		setUser(null)
-	}
-
-	console.log(user)
-
-	if (user) {
-		return (
-			<div className="App">
-				<h1>Hello, {user.email}</h1>
-				<button onClick={signOut}>Sign out</button>
-				<button onClick={getSession}>get session</button>
-			</div>
-		)
-	}
 	return (
 		<div className="App">
-			<h1>Hello, please sign in!</h1>
+			Расчет коммунальных платежей
+			<NumericInput
+				ref={inputRef}
+				onClick={() => {}}
+				isCorrect
+				maxLength={6}
+				message=""
+				minLength={6}
+				name=""
+				placeholder=""
+				validateStatus="validating"
+				onChange={setIndexInput}
+				value={indexInput}
+			/>
+			{/* <h1>Hello, please sign in!</h1>
 			<button onClick={signInWithGithub}>Sign In</button>
-			<button onClick={getSession}>get session</button>
+			<button onClick={getSession}>get session</button> */}
 		</div>
 	)
 }
@@ -75,8 +44,8 @@ export const Router = () => {
 		<RouterProvider
 			router={createBrowserRouter([
 				{
-					path: '/',
-					element: <App />,
+					path: '/counters',
+					element: <Counters />,
 				},
 			])}
 		/>
